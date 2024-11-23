01fdf27c7ac31700f9b6974f1cdeb3d3112a527b
songhee1 <ghenrhkwk88@naver.com>
2024-11-23 16:42:56 +0900

    1장 프로그래밍

---
+++ Chapter01/src/testCode/CalculatorTest.java
@@ -0,0 +1,21 @@
+package testCode;
+
+public class CalculatorTest {
+    public int add(int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+    }
+    public int subtract(int num1, int num2){
+        int sum = num1-num2;
+        return sum;
+    }
+
+    public int multiply(int num1, int num2){
+        int sum = num1*num2;
+        return sum;
+    }
+    public int divide(int num1, int num2){
+        int sum = num1*num2;
+        return sum;
+    }
+}
