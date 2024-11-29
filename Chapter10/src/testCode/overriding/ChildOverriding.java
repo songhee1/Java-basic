51b49e46cf4ade240882e9cea37af0817b7b50ca
songhee1 <ghenrhkwk88@naver.com>
2024-11-29 23:17:11 +0900

    상속 오버라이딩-접근제어자 축소

--- Chapter10/src/testCode/overriding/ChildOverriding.java
+++ Chapter10/src/testCode/overriding/ChildOverriding.java
@@ -6,5 +6,12 @@ public class ChildOverriding extends ParentOverriding{
     }
     public void printName(){
         System.out.println("ChildOverriding printName()");
-    }
+    } // 컴파일 에러, 같은 이름 메서드 부모클래스에 존재
+    /* 1. public String printName(){
+        System.out.println("ChildOverriding printName()");
+    } // 컴파일 에러, 같은 이름 메서드 부모클래스에 존재*/
+   /* 2. private void printName(){
+       System.out.println("ChildOverriding printName()");
+   } // 컴파일 에러, 접근제어자 범위 축소*/
+
 }
