924d704529057207dc1b77917330d057450c4ae8
songhee1 <ghenrhkwk88@naver.com>
2024-11-30 21:13:05 +0900

    equals(), hashCode()

--- Chapter12/src/testCode/equals/Equals.java
+++ Chapter12/src/testCode/equals/Equals.java
@@ -6,6 +6,7 @@ public class Equals {
         Equals thisObject = new Equals();
         thisObject.equalMethod();
         thisObject.equalMethod2();
+        thisObject.printHashCode();
     }
     public void equalMethod(){
         MemberDTO obj1 = new MemberDTO("Sangmin");
@@ -22,10 +23,19 @@ public class Equals {
         MemberDTO obj1 = new MemberDTO("Sangmin");
         MemberDTO obj2 = new MemberDTO("Sangmin");
 
-        if(obj1.equals(obj2)){ // hashCode()(주소값) 비교
-            System.out.println("obj1 and obj2 is same");
+        if(obj1.equals(obj2)){
+            System.out.println("obj1 and obj2 is same"); // equals 오버라이딩시 출력
         }else{
-            System.out.println("obj1 and obj2 is different"); // 출력
+            System.out.println("obj1 and obj2 is different"); // equals 오버라이딩 안했을 경우 출력, == 주소값 비교
         }
+    }
+
+    public void printHashCode(){
+        MemberDTO obj1 = new MemberDTO("Sangmin");
+        MemberDTO obj2 = new MemberDTO("Sangmin");
+
+        System.out.println(obj1.hashCode());
+        System.out.println(obj2.hashCode());
+        // hashCode 오버라이딩시 같은 int값
     }
 }
--- Chapter12/src/testCode/equals/MemberDTO.java
+++ Chapter12/src/testCode/equals/MemberDTO.java
@@ -1,5 +1,7 @@
 package testCode.equals;
 
+import c.inheritance.Student;
+
 public class MemberDTO {
     public String name;
     public String phone;
@@ -24,13 +26,36 @@ public class MemberDTO {
             '}';
     }
 
-  /* @Override
+  @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
+       if (this == obj) return true; // 참조값이 같은 경우 true 리턴
+       if (obj == null || getClass() != obj.getClass()) return false; // 비교하는 객체가 null이거나 클래스 불일치 시 false 리턴
+
+       MemberDTO other = (MemberDTO) obj;
+       if(name == null){
+           if(other.name != null) return false;
+       }else if(!name.equals(other.name)) return false;
+
+       if(phone == null){
+          if(other.phone != null) return false;
+      }else if(!phone.equals(other.phone)) return false;
+
+       if(email == null){
+          if(other.email != null) return false;
+      }else if(!email.equals(other.email)) return false;
+
+       return true;
+
+  }
 
     @Override
-    public int hashCode() {
-        return super.hashCode();
-    }*/
+    public int hashCode() { // equals 오버라이딩시 함께 오버라이딩
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+
+        return result;
+    }
 }
