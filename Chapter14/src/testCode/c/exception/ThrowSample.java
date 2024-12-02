e3b6f16798b4716c83e5cd9a4bfdd312be814a6a
songhee1 <ghenrhkwk88@naver.com>
2024-12-02 11:09:10 +0900

    throws시 주의할점

--- Chapter14/src/testCode/c/exception/ThrowSample.java
+++ Chapter14/src/testCode/c/exception/ThrowSample.java
@@ -2,30 +2,24 @@ package testCode.c.exception;
 
 public class ThrowSample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception { // main()에서 에러 메서지
         ThrowSample sample = new ThrowSample();
         sample.throwException(13);
         System.out.println("===");
-//        sample.throwsException(13); // throwsException에서 예외 throw 했기에, 1. try-catch 2. throws 필요
+        sample.throwsException(13); // throwsException에서 예외 throw 했기에, 1. try-catch 2. throws 필요
 
-        // 1.
-        try{
-            sample.throwsException(13);
-        }catch (Exception e){
-
-        }
     }
     public void throwException(int number){
         try{
             if(number>12){
-                throw new Exception("Number is over than 12");
+                throw new Exception("Number is over than 12"); // 예외 발생시 예외 클래스 객체 생성
 
             }
             System.out.println("Number is "+number);
         }catch (Exception e){
             e.printStackTrace(); //수행
         }
-        //해당하는 예외없다면 메서드를 호출한 메서드로 던짐
+        //해당하는 예외없다면 메서드를 호출한 메서드로 던짐(예외를 위임함)-컴파일에러
     }
     public void throwsException(int number) throws Exception{
         if(number>12){
@@ -33,5 +27,8 @@ public class ThrowSample {
         }
         System.out.println("Number is "+number);
     }
+    // throw 예외 던질 경우, 해당 메서드에서 try-catch/throws 미작성시 컴파일에러
+    public void multiThrows() throws NullPointerException, Exception{ // 예외 클래스 나열 가능
 
+    }
 }
