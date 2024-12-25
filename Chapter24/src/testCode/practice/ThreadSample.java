842fee34bd0cc2d5f72a3e043051e65a8a42fe9c
songhee1 <ghenrhkwk88@naver.com>
2024-12-25 17:35:10 +0900

    synchronized로 공유코드문제점 해결

--- Chapter24/src/testCode/practice/ThreadSample.java
+++ Chapter24/src/testCode/practice/ThreadSample.java
@@ -1,22 +1,19 @@
 package testCode.practice;
 
+import com.sun.tools.javac.Main;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ThreadSample {
     private static int value = 0;
     public static void main(String[] args) throws InterruptedException {
-        Thread t = new MyThread(); //Thread 상속받는 방법
-        t.start();
-
-        Thread t2 = new Thread(new MyThread2());// Runnable 구현하는 방법
-        t2.start();
-
         List<Thread> pool = new ArrayList<>();
         for(int i=0;i<10;i++){
             Thread t3 = new Thread(() -> {
                 for(int j=0;j<1000000;j++){
-                    value++;
+                    synchronized (ThreadSample.class){
+                        value++;
+                    }
                 }
             });
 
@@ -31,17 +28,4 @@ public class ThreadSample {
         System.out.println("value = "+value);
     }
 }
-// 스레드 생성방법
-class MyThread extends Thread{
-    @Override
-    public void run(){
-    }
-}
 
-class MyThread2 implements Runnable{
-
-    @Override
-    public void run() {
-
-    }
-}
