c2e3df07da4d32a0fdef9754d85854047f42e46a
songhee1 <ghenrhkwk88@naver.com>
2024-12-25 17:00:58 +0900

    공유 코드 부분일때 멀티스레드 문제

--- Chapter24/src/testCode/practice/ThreadSample.java
+++ Chapter24/src/testCode/practice/ThreadSample.java
@@ -1,12 +1,34 @@
 package testCode.practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreadSample {
-    public static void main(String[] args) {
+    private static int value = 0;
+    public static void main(String[] args) throws InterruptedException {
         Thread t = new MyThread(); //Thread 상속받는 방법
         t.start();
 
         Thread t2 = new Thread(new MyThread2());// Runnable 구현하는 방법
         t2.start();
+
+        List<Thread> pool = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            Thread t3 = new Thread(() -> {
+                for(int j=0;j<1000000;j++){
+                    value++;
+                }
+            });
+
+            t3.start();
+            pool.add(t3);
+        }
+
+        for(Thread t3 : pool){
+            t3.join();
+        }
+
+        System.out.println("value = "+value);
     }
 }
 // 스레드 생성방법
